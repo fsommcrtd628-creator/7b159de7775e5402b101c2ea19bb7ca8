50f0ece2bf6cc3751b6aec3a361a2b1dfc7fccc5
鈴木新 <arata@suzukishinnoiMac.local>
2026-07-26 10:42:13 +0900

    animation :fix

--- script.js
+++ script.js
@@ -9,36 +9,95 @@ const messages = [
     "何か当ててみてください<br>（当たった場合、あげます）"
 ];
 
-
 let index = 0;
+let animating = false;
 
 function showMessage() {
-    title.style.opacity = 0;
+
     title.innerHTML = messages[index];
 
-    title.animate(
-        [
-            { opacity: 0, transform: "scale(0.5)" },
-            { opacity: 1, transform: "scale(1)" }
-        ],
+    title.animate([
         {
-            duration: 1000,
-            fill: "forwards"
+            opacity: 0,
+            transform: "translateY(80px)"
+        },
+        {
+            opacity: 1,
+            transform: "translateY(0)"
         }
-    );
+    ], {
+        duration: 700,
+        fill: "forwards",
+        easing: "ease-out"
+    });
 
-        // 最後のメッセージなら案内を消す
     if (index === messages.length - 1) {
         hint.style.display = "none";
     }
 
-  }
+    animating = false;
+}
 
 showMessage();
 
 document.addEventListener("click", () => {
-    if (index < messages.length - 1) {
+
+    if (animating || index >= messages.length - 1) return;
+
+    animating = true;
+
+    title.animate([
+        {
+            opacity: 1,
+            transform: "translateY(0)"
+        },
+        {
+            opacity: 0,
+            transform: "translateY(-80px)"
+        }
+    ], {
+        duration: 500,
+        fill: "forwards",
+        easing: "ease-in"
+    });
+
+    setTimeout(() => {
         index++;
         showMessage();
-    }
+    }, 500);
+
 });
+
+
+// let index = 0;
+
+// function showMessage() {
+//     title.style.opacity = 0;
+//     title.innerHTML = messages[index];
+
+//     title.animate(
+//         [
+//             { opacity: 0, transform: "scale(0.5)" },
+//             { opacity: 1, transform: "scale(1)" }
+//         ],
+//         {
+//             duration: 1000,
+//             fill: "forwards"
+//         }
+//     );
+
+//         // 最後のメッセージなら案内を消す
+//     if (index === messages.length - 1) {
+//         hint.style.display = "none";
+//     }
+
+//   }
+
+// showMessage();
+
+// document.addEventListener("click", () => {
+//     if (index < messages.length - 1) {
+//         index++;
+//         showMessage();
+//     }
+// });
